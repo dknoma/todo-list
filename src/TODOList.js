bdb58ec4f03d846a9123cd8eaf075493f06f1f2f
dknoma <dknoma@dons.usfca.edu>
2018-06-05 14:22:42 -0700

    creating the server for the list

--- src/TODOList.js
+++ src/TODOList.js
@@ -1,1 +1,10 @@
+//include http module
 var http = require('http');
+
+// Create the server. Function passed as parameter is called on every request made.
+// request variable holds all request parameters
+// response variable allows you to do anything with response sent to the client.
+
+http.createServer(function (request, response) {
+    
+});
